59bc183f1d328a82d0ba9dcfcaa49995d16200c4
Ivan Lazaric <ivan.lazaric1@gmail.com>
2019-06-27 10:52:40 +0200

    save

--- source/neural.py
+++ source/neural.py
@@ -29,7 +29,7 @@ mini_batch_size = 32
 
 cnn = Sequential()
 
-cnn.add(Conv2D(filters=10, 
+cnn.add(Conv2D(filters=20, 
                kernel_size=(3,3), 
                padding='valid',
                input_shape=(H,W,3),
@@ -47,7 +47,7 @@ cnn.add(Activation('relu'))
 cnn.add(MaxPooling2D(pool_size=(2,2),
                      border_mode='valid'))
 
-cnn.add(Conv2D(filters=60,
+cnn.add(Conv2D(filters=40,
                kernel_size=(6,6),
                use_bias=True,
                padding='valid'))
@@ -57,12 +57,12 @@ cnn.add(MaxPooling2D(pool_size=(2,2),
 
 cnn.add(Flatten())
 
-cnn.add(Dense(32, use_bias=True, activity_regularizer=keras.regularizers.l2(0.0001)))
+cnn.add(Dense(32, use_bias=True))
 cnn.add(Activation('relu'))
 cnn.add(Dropout(0.25))
 
-cnn.add(Dense(1, use_bias=True, activity_regularizer=keras.regularizers.l2(0.0001)))
-cnn.add(Activation('sigmoid'))
+cnn.add(Dense(1))
+cnn.add(Activation('linear'))
 
 optimizer = keras.optimizers.Adam()
 cnn.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['mean_absolute_error'])
@@ -70,15 +70,17 @@ cnn.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['mean_absol
 start = time.time()
 cnn.fit(
     x = np.array([region["img"] for regions in training for region in regions]),
-    y = np.array([region["count"] for regions in training for region in regions])/5,
+    y = np.array([region["count"] for regions in training for region in regions]),
     batch_size = mini_batch_size,
-    epochs = 10,
-    validation_split = 0.1
+    epochs = 5,
+    validation_split = 0.2
 )
 end = time.time()
 print('Processing time:',(end - start)/60)
 # cnn.save_weights('cnn_baseline.h5')
 
+avg_rel = 0
+cnt = 0
 for img in open_images("../images/test_images"):
     # predictions = cnn.predict(x=np.array([region["img"] for region in img]), batch_size=mini_batch_size) * 5
     # prediction = 0
@@ -95,8 +97,15 @@ for img in open_images("../images/test_images"):
         for j in range(0, height-H+1, 15):
             args.append([np.asarray(img["img"].crop([i, j, i+W, j+H]))/255])
     predictions = cnn.predict(x=np.array(args).reshape((len(args), H, W, 3)), batch_size=mini_batch_size)
-    for p in predictions: prediction += p*5
+    for p in predictions: prediction += p
     prediction *= (width/W) * (height/H) / len(args)
     actual = len(img["loc"]["X"])
     print(img["name"], prediction, actual, abs(prediction/actual - 1))
+    cnt += 1
+    avg_rel += abs(prediction/actual - 1)
     
+print(avg_rel/cnt)
+
+cnn.summary()
+
+cnn.save('cnn.h5')
